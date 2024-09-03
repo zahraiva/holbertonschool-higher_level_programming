61de9c9bfe564d4e502c261376fc3723b4f04ae3
zahraiva <zaraiv06@gmail.com>
2024-09-03 20:42:09 +0400

    fixx

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for i in range(1, 100):
     if i % 10 > i // 10:
-        print("{:02d}, ".format(i), end="")
-        
+        if i < 89:
+            print("{:02d}, ".format(i), end="")
+        else:
+            print("{:02d}".format(i))
