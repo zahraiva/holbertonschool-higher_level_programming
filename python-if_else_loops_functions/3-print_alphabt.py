b442f38f31cc5e8bc1ad7ef79c90f1ccae5c9a22
zahraiva <zaraiv06@gmail.com>
2024-09-03 20:11:00 +0400

    pycodestyle fixed

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import string
-alp = string.ascii_lowercase.replace('e', '').replace('q','')
+alp = string.ascii_lowercase.replace('e', '').replace('q', '')
 for i in alp:
     print("{}".format(alp), end="")
     break
