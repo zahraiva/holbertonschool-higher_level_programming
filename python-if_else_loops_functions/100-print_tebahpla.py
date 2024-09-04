2192bace8477fcf8952d3ca3e73160b41b86bf9d
zahraiva <zaraiv06@gmail.com>
2024-09-04 12:17:29 +0400

    advanced task

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def reversed_alp():
+    res = ''
+    for i in range(25, -1, -1):
+        char = chr(122 - i)
+        if i % 2 == 0:
+            res += char
+        else:
+            res += char.upper()
+    print(res)
+reversed_alp()
