a5eb5b659be36a835cbd585d963b45c9ca15b85f
zahraiva <zaraiv06@gmail.com>
2024-09-07 23:55:21 +0400

    2 task

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     if num == 0:
         print("0 arguments.")
     elif num == 1:
-        print("1 argument")
+        print("1 argument:")
     else:
-        print("{} arguments".format(num))
+        print("{} arguments:".format(num))
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
