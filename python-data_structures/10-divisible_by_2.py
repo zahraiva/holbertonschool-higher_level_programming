b10f6da98aebefd5b8afc6eb9ec5b2892e4ded81
zahraiva <zaraiv06@gmail.com>
2024-09-10 19:59:13 +0400

    main deleted

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -7,13 +7,3 @@ def divisible_by_2(my_list=[]):
         else:
             new_list.append(False)
     return new_list
-
-
-if __name__ == '__main__':
-    my_list = [0, 1, 2, 3, 4, 5, 6]
-    list_result = divisible_by_2(my_list)
-
-    i = 0
-    while i < len(list_result):
-        print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
-        i += 1
