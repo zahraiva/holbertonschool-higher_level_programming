7660aece602dafda03810b34d1f6667bbdcb5d7c
zahraiva <zaraiv06@gmail.com>
2024-09-10 19:39:33 +0400

    8 task

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence):
+        return len(sentence), sentence[0]
+    return None
+
+
+if __name__ == '__main__':
+    sentence = "At school, I learnt C!"
+    length, first = multiple_returns(sentence)
+    print("Length: {:d} - First character: {}".format(length, first))
