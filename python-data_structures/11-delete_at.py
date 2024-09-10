c33204b9885e57ea2ba238a3619e126188ac1ce9
zahraiva <zaraiv06@gmail.com>
2024-09-10 20:14:02 +0400

    11 task

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    new_list = []
+    for element in range(len(my_list)):
+        if element != idx:
+            new_list.append(my_list[element])    
+    del my_list[idx]
+    return new_list
+
+
+if __name__ == '__main__':
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    new_list = delete_at(my_list, idx)
+    print(new_list)
+    print(my_list)
