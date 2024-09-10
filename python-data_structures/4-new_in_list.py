e8e40c0264b6daa0872c4f25cbf12b23ddad3fc0
zahraiva <zaraiv06@gmail.com>
2024-09-10 17:07:58 +0400

    4 task

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    new_list = my_list.copy()
+    new_list[idx] = new_element
+    return new_list
+
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    new_element = 9
+    new_list = new_in_list(my_list, idx, new_element)
+
+    print(new_list)
+    print(my_list)
