7f2401a206d407bf76c637460e5d22c118dc8df9
zahraiva <zaraiv06@gmail.com>
2024-09-10 18:50:25 +0400

    6 task

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for i in matrix:
+            for j in i:
+                print('{:d}'.format(j), end=' ')
+            print()
+
+
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    print_matrix_integer(matrix)
+    print("--")
+    print_matrix_integer()
