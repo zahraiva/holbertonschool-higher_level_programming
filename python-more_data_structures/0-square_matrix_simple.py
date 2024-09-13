ce16cc2572940f0ce89ee55d3bef1d89624f1990
zahraiva <zaraiv06@gmail.com>
2024-09-13 18:38:15 +0400

    0 task

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for x in matrix:
+        temp = []
+        for y in x:
+            temp.append(y**2)
+        new_matrix.append(temp)    
+        
+    return new_matrix
+
+
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    new_matrix = square_matrix_simple(matrix)
+    print(new_matrix)
+    print(matrix)
