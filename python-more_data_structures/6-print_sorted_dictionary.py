46a9894b1250861afaeabdf5562c660032c3d940
zahraiva <zaraiv06@gmail.com>
2024-09-13 19:01:59 +0400

    6 task

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sort_olunmus = sorted(a_dictionary.items())
+    for key, val in sort_olunmus:
+        print("{}: {}".format(key, val))
+
+
+if __name__ == "__main__":
+    a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+    print_sorted_dictionary(a_dictionary)
