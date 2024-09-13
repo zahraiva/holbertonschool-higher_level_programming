09313eadda0659e0123c3d00fd631f144b776a1a
zahraiva <zaraiv06@gmail.com>
2024-09-13 19:00:09 +0400

    5 task

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -4,6 +4,6 @@ def number_keys(a_dictionary):
 
 
 if __name__ == "__main__":
-    a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level" }
+    a_dictionary = {'language': "C", 'number': 13, 'track': "Low level"}
     nb_keys = number_keys(a_dictionary)
     print("Number of keys: {:d}".format(nb_keys))
