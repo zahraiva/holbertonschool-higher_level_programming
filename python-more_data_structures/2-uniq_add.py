9c8087aeeac5c6d1709155e411ee27f4fc701a92
zahraiva <zaraiv06@gmail.com>
2024-09-13 18:48:16 +0400

    2 task fix

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,7 +3,7 @@ def uniq_add(my_list=[]):
     return sum(set(my_list))
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     uniq_add = __import__('2-uniq_add').uniq_add
 
     my_list = [1, 2, 3, 1, 4, 2, 5]
