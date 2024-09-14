2f9fe6da071fbc5166f1dd43a382610b742f82e3
zahraiva <zaraiv06@gmail.com>
2024-09-14 12:41:07 +0400

    9 task

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dict = {}
+    for k, v in a_dictionary.items():
+        new_dict[k] = v * 2
+    return new_dict 
+
+
+if __name__ == "__main__":
+    a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
+    new_dict = multiply_by_2(a_dictionary)
+    print_sorted_dictionary(a_dictionary)
+    print("--")
+    print_sorted_dictionary(new_dict)
