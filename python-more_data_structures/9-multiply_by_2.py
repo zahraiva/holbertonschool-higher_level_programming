477880743d2f94b989ebfa8793ade95ae354fff1
zahraiva <zaraiv06@gmail.com>
2024-09-14 12:42:37 +0400

    9 task fix

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -3,7 +3,7 @@ def multiply_by_2(a_dictionary):
     new_dict = {}
     for k, v in a_dictionary.items():
         new_dict[k] = v * 2
-    return new_dict 
+    return new_dict
 
 
 if __name__ == "__main__":
