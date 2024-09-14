f078b187bfcba833ec5514a932cf9a8ab94a09e6
zahraiva <zaraiv06@gmail.com>
2024-09-14 13:06:00 +0400

    avdanced task 1

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    sum1 = 0
+    sum2 = 0
+    for i in my_list:
+        sum1 += i[0] * i[1]
+        sum2 += i[1]
+    return sum1 / sum2
