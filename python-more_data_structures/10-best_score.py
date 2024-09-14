f5bccbac2947cea56bda054b6fb4ee5b1b0fc261
zahraiva <zaraiv06@gmail.com>
2024-09-14 12:54:41 +0400

    10 task fix

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
-        return a_dictionary
-    else:
+    if not a_dictionary:
         return None
     return max(a_dictionary, key=a_dictionary.get)
-
-
-if __name__ == "__main__":
-    best_key = best_score(a_dictionary)
-    print("Best score: {}".format(best_key))
-
-    best_key = best_score(None)
-    print("Best score: {}".format(best_key))
