091b925bc1770c0b542125ef4a905c8fb99f7f24
zahraiva <zaraiv06@gmail.com>
2024-09-21 19:25:20 +0400

    task 2 fix

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,6 +6,15 @@ class Square:
     """Square classssss"""
 
     def __init__(self, size=0):
+        """_summary_
+
+        Args:
+            size (int, optional): _description_. Defaults to 0.
+
+        Raises:
+            TypeError: _description_
+            ValueError: _description_
+        """
         if not isinstance:
             raise TypeError('size must be an integer')
         elif size < 0:
