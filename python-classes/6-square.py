27bc0f04eb895a4b6811977f0850c99056517ac6
zahraiva <zaraiv06@gmail.com>
2024-09-22 17:09:10 +0400

    task 6 fixxxxxxx

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -4,34 +4,39 @@
 
 class Square:
     """Square classssss"""
-
-    def __init__(self, size=0, position=(0, 0)):
-        self.__position = position
+    def __init__(self, size=0, position=(0, 0)) -> None:
+        """Initialize Square with size attribute"""
         if not isinstance(size, int):
-            raise TypeError('size must be an integer')
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError('size must be >= 0')
+            raise ValueError("size must be >= 0")
+        if not isinstance(position, tuple) or len(position) != 2 or \
+                not all(isinstance(i, int) for i in position) or \
+                not all(i >= 0 for i in position):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__size = size
+        self.__position = position
+
+    def area(self):
+        return self.__size ** 2
 
     @property
     def size(self):
         return self.__size
 
     @size.setter
-    def size(self, size):
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = size
-
-    def area(self):
-        return self.__size ** 2
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     @property
     def position(self):
         return self.__position
-    
+
     @position.setter
     def position(self, value):
         if not isinstance(value, tuple) or len(value) != 2 or \
