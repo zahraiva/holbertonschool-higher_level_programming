53159d09105121388cf0ac742d8ab98ff710ba12
zahraiva <zaraiv06@gmail.com>
2024-09-24 23:47:50 +0400

    task 7 fix

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -14,5 +14,3 @@ class BaseGeometry():
             raise TypeError('{} must be an integer'.format(name))
         elif value < 0:
             raise ValueError('{} must be greater than 0'.format(name))
-        self.name = name
-        self value = value
