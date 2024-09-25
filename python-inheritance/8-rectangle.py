b7d3ad9e8084a62644b4e64b3701625a86502aab
zahraiva <zaraiv06@gmail.com>
2024-09-25 13:20:09 +0400

    task 8 fixxx

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -16,12 +16,7 @@ class BaseGeometry():
             raise ValueError('{} must be greater than 0'.format(name))
 
     def __init__(self, width, height):
-        """_summary_
-
-        Args:
-            width (_type_): _description_
-            height (_type_): _description_
-        """
+        """init a rectangle object"""
         self.integer_validator('width', width)
         self.__width = width
         self.integer_validator('height', height)
