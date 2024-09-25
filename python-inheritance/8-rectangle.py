aa1078f8f7a8e1db6ea5379a76856b704495a92a
zahraiva <zaraiv06@gmail.com>
2024-09-25 13:13:39 +0400

    task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""an empty class"""
+
+
+class BaseGeometry():
+    """class basegeometry"""
+    def area(self):
+        """to raise an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """to validate value"""
+        if type(value) is not int:
+            raise TypeError('{} must be an integer'.format(name))
+        elif value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+
+    def __init__(self, width, height):
+        self.integer_validator('width' width)
+        self.__width = width
+        self.integer_validator('height' height)
+        self.__height = height
