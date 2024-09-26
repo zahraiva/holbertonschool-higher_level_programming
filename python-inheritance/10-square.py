eeb90df43a9c885d8645d633310ab1016026373c
zahraiva <zaraiv06@gmail.com>
2024-09-26 12:29:10 +0400

    task 10 fixed

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Module containing Rectangle class"""
-Rectangle = __import__('9-rectangle.py').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """class Square with parent Rectangle"""
+    """class Square with a parent Rectangle"""
     def __init__(self, size):
         self.integer_validator('size', size)
         self.__size = size
