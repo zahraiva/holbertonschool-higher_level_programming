f677197c6da3644c46aa28904b1284f26b738fea
zahraiva <zaraiv06@gmail.com>
2024-09-28 11:15:04 +0400

    task 3

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 
 
-class CounterIterator:
+class CountedIterator:
+
     def __init__(self, iterable):
         self.iterator = iter(iterable)
-        self.counter = 0
+        self.count = 0
 
     def __next__(self):
         try:
             item = next(self.iterator)
-            self.counter += 1
+            self.count += 1
             return item
-
         except StopIteration:
             raise StopIteration
 
     def get_count(self):
-        return self.counter
+        return self.count
