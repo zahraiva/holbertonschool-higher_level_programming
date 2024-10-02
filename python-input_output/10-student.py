b61cfd8e877f5c9d4856f9d9c536e28146df4dbd
zahraiva <zaraiv06@gmail.com>
2024-10-02 18:34:54 +0400

    task 10 eeeee

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""student to JSON"""
+"""Module containing Student class"""
 
 
 class Student:
-    """student class"""
+    """The Student class"""
 
     def __init__(self, first_name, last_name, age):
-        """initialization of our class"""
+        """Initialization of the class instance"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-    """retrieving dict elements of student class"""
-    if isinstance(attrs, list) and all(isinstance(element, str) for element in attrs):
-        new_dict = {}
-        for i in attrs:
-            if i in self.__dict__:
-                new_dict[i] = self.__dict__[i]
-        return new_dict
-    return self.__dict__
+        """Method to retrieve dict of the class"""
+        if isinstance(attrs, list) and \
+                all(isinstance(element, str) for element in attrs):
+            new_dict = dict()
+            for i in attrs:
+                if i in self.__dict__:
+                    new_dict[i] = self.__dict__[i]
+            return new_dict
+        return self.__dict__
