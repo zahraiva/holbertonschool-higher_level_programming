9b7582a6ff143f5b54f444b97a9fc0c5c8b1acaa
zahraiva <zaraiv06@gmail.com>
2024-10-02 18:36:45 +0400

    task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module containing Student class"""
+
+
+class Student:
+    """The Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization of the class instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Method to retrieve dict of the class"""
+        if isinstance(attrs, list) and \
+                all(isinstance(element, str) for element in attrs):
+            new_dict = dict()
+            for i in attrs:
+                if i in self.__dict__:
+                    new_dict[i] = self.__dict__[i]
+            return new_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """changing JSON"""
+        for key in self.__dict__.keys():
+            for key2 in json.keys():
+                if key == key2:
+                    self.__dict__[key] = json[key2]
