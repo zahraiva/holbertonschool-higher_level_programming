47b9a06552b963c3f64e7d79ef934fd7a8cd244a
zahraiva <zaraiv06@gmail.com>
2024-10-02 17:47:10 +0400

    2 fixxx

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -4,5 +4,5 @@
 
 def append_write(filename="", text=""):
     """append write"""
-    with open(filename, 'a+r', encoding='utf-8') as a:
+    with open(filename, 'a', encoding='utf-8') as a:
         return a.write(text)
