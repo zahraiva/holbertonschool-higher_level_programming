660d3126ec4d2f4b777dd578fa9e41d73219df7a
zahraiva <zaraiv06@gmail.com>
2024-10-02 18:29:42 +0400

    taskkkk 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -11,7 +11,12 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        if attrs is None:
-            return self.__dict__
-        return {attr: self.__dict__[attr] for attr in attrs if attr in self.__dict__}
+     def to_json(self, attrs=None):
+        if isinstance(attrs, list) and \
+                all(isinstance(element, str) for element in attrs):
+            new_dict = dict()
+            for i in attrs:
+                if i in self.__dict__:
+                    new_dict[i] = self.__dict__[i]
+            return new_dict
+        return self.__dict__
