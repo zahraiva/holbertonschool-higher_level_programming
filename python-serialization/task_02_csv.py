d0b640026232646d0766275ebcdd057c35469abe
zahraiva <zaraiv06@gmail.com>
2024-10-05 20:20:28 +0400

    task 2

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import csv
+import json
+
+
+def convert_csv_to_json(file_csv):
+    """converting csv to json"""
+    try:
+        with open(file_csv, 'r') as a:
+            csvr = csv.DictReader(a)
+            content = list(csvr)
+
+        with open('data.json', 'w') as a:
+            json.dump(content, a)
+
+        return True
+    except (FileNotFoundError, IOError) as e:
+        print(f'error:' {e})
+        return False
