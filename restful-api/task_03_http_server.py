9891ee9ffa36774f88633a26153950b0016b7026
zahraiva <zaraiv06@gmail.com>
2024-10-13 17:28:09 +0400

    im going crazyy

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -31,7 +31,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            error_response = {'error': 'Endpoint not found'}
+            error_response = {"error": "Endpoint not found"}
             self.wfile.write(json.dumps(error_response).encode())
 
 def main():
