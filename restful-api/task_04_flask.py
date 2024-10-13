464e9cc83e3b26cd1acb8887f010fd8e2c65e4a2
zahraiva <zaraiv06@gmail.com>
2024-10-13 18:43:34 +0400

    aa

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -57,4 +57,6 @@ def add_user():
     return jsonify({"message": "User added", "user": users[data['username']]})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    if __name__ == "__main__":
+    app.run(debug=True, use_reloader=False, use_debugger=False)
+
