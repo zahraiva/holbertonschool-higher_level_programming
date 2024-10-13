1ba39c21b88baaf647c1ceb61c6c18c67821c841
zahraiva <zaraiv06@gmail.com>
2024-10-13 17:11:35 +0400

    task 03

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,38 @@
+import http.server
+import json
+import socketserver
+
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a simple API!')
+
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
+            self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b'404 Not Found')
+
+with socketserver.TCPServer(("", 8000), MyHandler) as httpd:
+    print("Serving at port 8000")
+    httpd.serve_forever()
