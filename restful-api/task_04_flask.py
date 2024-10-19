486f0cb8ab5f3759f48bc634af6e93e40f10b5a7
zahraiva <zaraiv06@gmail.com>
2024-10-19 13:30:23 +0400

    *sobbing*

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -3,46 +3,46 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-all_users = {}
+user_data = {}
 
 
 @app.route('/')
-def home():
+def home_page():
     return 'Welcome to the Flask API!'
 
 
 @app.route('/data')
-def data():
-    return jsonify(list(all_users.keys()))
+def retrieve_usernames():
+    return jsonify(list(user_data.keys()))
 
 
 @app.route('/status')
-def status():
+def check_status():
     return "OK"
 
 
 @app.route('/users/<username>')
-def user(username):
-    if username in all_users:
-        return jsonify(all_users[username])
+def fetch_user(username):
+    if username in user_data:
+        return jsonify(user_data[username])
     return jsonify({"error": "User not found"}), 404
 
 
 @app.route('/add_user', methods=['POST'])
-def add_user():
-    data = request.get_json()
-    if not data or 'username' not in data:
+def register_user():
+    new_user = request.get_json()
+    if not new_user or 'username' not in new_user:
         return jsonify({"error": "Username is required"}), 400
-    if data['username'] in all_users:
+    if new_user['username'] in user_data:
         return jsonify({"error": "Username already exists"}), 400
 
-    all_users[data['username']] = {
-        "username": data['username'],
-        "name": data.get('name', ''),
-        "age": data.get('age', None),
-        "city": data.get('city', '')
+    user_data[new_user['username']] = {
+        "username": new_user['username'],
+        "name": new_user.get('name', ''),
+        "age": new_user.get('age', None),
+        "city": new_user.get('city', '')
     }
-    return jsonify({"message": "User added", "user": all_users[data['username']]}), 201
+    return jsonify({"message": "User added", "user": user_data[new_user['username']]}), 201
 
 
 if __name__ == "__main__":
