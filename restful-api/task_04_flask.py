798645fc89c5824e7ff8a366e0a9b8cca1154bc9
zahraiva <zaraiv06@gmail.com>
2024-10-19 13:32:21 +0400

    aaaa

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -3,47 +3,46 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-user_data = {}
+users_data = {}
 
 
 @app.route('/')
-def home_page():
+def welcome_message():
     return 'Welcome to the Flask API!'
 
 
 @app.route('/data')
-def retrieve_usernames():
-    return jsonify(list(user_data.keys()))
-
-
-@app.route('/status')
-def check_status():
-    return "OK"
-
-
-@app.route('/users/<username>')
-def fetch_user(username):
-    if username in user_data:
-        return jsonify(user_data[username])
-    return jsonify({"error": "User not found"}), 404
+def list_users():
+    return jsonify(list(users_data.keys()))
 
 
 @app.route('/add_user', methods=['POST'])
-def register_user():
-    new_user = request.get_json()
-    if not new_user or 'username' not in new_user:
-        return jsonify({"error": "Username is required"}), 400
-    if new_user['username'] in user_data:
-        return jsonify({"error": "Username already exists"}), 400
-
-    user_data[new_user['username']] = {
-        "username": new_user['username'],
-        "name": new_user.get('name', ''),
-        "age": new_user.get('age', None),
-        "city": new_user.get('city', '')
+def create_user():
+    user_info = request.json
+    if user_info is None or 'username' not in user_info:
+        return jsonify({'error': 'Username is required'}), 400
+    
+    user_info['username'] = user_info.get('username')
+    users_data[user_info['username']] = {
+        'username': user_info['username'],
+        'name': user_info.get('name'),
+        'age': user_info.get('age'),
+        'city': user_info.get('city')
     }
-    return jsonify({"message": "User added", "user": user_data[new_user['username']]}), 201
+    return jsonify({'message': 'User added successfully', 'user': users_data[user_info['username']]}), 201
+
+
+@app.route('/users/<username>')
+def retrieve_user(username):
+    if username not in users_data:
+        return jsonify({"error": "User not found"}), 404
+    return jsonify(users_data[username])
+
+
+@app.route('/status')
+def api_status():
+    return "OK"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
