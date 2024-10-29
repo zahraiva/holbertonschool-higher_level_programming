555abf5391c33cd89b578396b308a10bf5dc5e31
zahraiva <zaraiv06@gmail.com>
2024-10-29 16:43:17 +0000

    done

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,5 @@
 SELECT tv_shows.title tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
-ON tv_show_genres.genre_id = tv_shows.id
+ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title tv_show_genres.genre_id;
