45c5afc1b6d0b3bb55524f872d788e321bd9a7d4
zahraiva <zaraiv06@gmail.com>
2024-10-29 15:34:01 +0000

    done

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- listing info in a database
-SELECT id, name FROM cities, WHERE cities.id IN (SELECT id FROM states WHERE name = California) ORDER BY id ASC;
+SELECT id, name FROM cities WHERE cities.id IN (SELECT id FROM states WHERE name = California) ORDER BY id ASC;
