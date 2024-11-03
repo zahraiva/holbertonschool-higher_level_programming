5223eeb19321627c8d9c83cb8328382a988969c7
zahraiva <zaraiv06@gmail.com>
2024-11-03 16:40:48 +0400

    done

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- t listing all shows contained in hbtn_0d_tvshows without a genre linked
-SELECT tv_shows.title tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title tv_show_genres.genre_id;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,4 +3,4 @@ SELECT genres_name AS genre COUNT(tv_shows.id) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genres.name
-ORDER BY number_of_shows
+ORDER BY number_of_shows;
