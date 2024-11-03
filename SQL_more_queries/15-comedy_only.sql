a4c28b4a4bba91b993a371996f22a177452f5cb1
zahraiva <zaraiv06@gmail.com>
2024-11-03 17:07:14 +0400

    done

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,7 @@
 -- sql task
 SELECT tv_shows.title AS title
-FROM tv_genres INNER JOIN tv_shows_genres ON tv_genres.id = tv_show_genres.genre_id
+FROM tv_genres
+INNER JOIN tv_shows_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_genres.name = "Comedy"
-ORDER BY tv_shows.title
+ORDER BY tv_shows.title;
